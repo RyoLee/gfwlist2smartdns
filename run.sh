dc59d068be383a35e4cee672fe785d64c0e85cc8
Lee <516127941@qq.com>
2022-10-03 19:44:32 +0800

    fix: cdn

--- run.sh
+++ run.sh
@@ -6,16 +6,16 @@ while getopts 'o:' OPT; do
     *) echoerr "Unknown error while processing options";;
   esac
 done
-curl -sS https://raw.githubusercontents.com/gfwlist/gfwlist/master/gfwlist.txt | \
+curl -sS https://ghproxy.com/https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt | \
   base64 -d | sort -u | sed '/^$\|@@/d'| sed 's#!.\+##; s#|##g; s#@##g; s#http:\/\/##; s#https:\/\/##;' | \
   sed '/apple\.com/d; /sina\.cn/d; /sina\.com\.cn/d; /baidu\.com/d; /qq\.com/d' | \
   sed '/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$/d' | grep '^[0-9a-zA-Z\.-]\+$' | \
   grep '\.' | sed 's#^\.\+##' | sort -u > /tmp/temp_gfwlist1
 
-curl -sS https://raw.githubusercontents.com/hq450/fancyss/master/rules/gfwlist.conf | \
+curl -sS https://ghproxy.com/https://raw.githubusercontent.com/hq450/fancyss/master/rules/gfwlist.conf | \
   sed 's/ipset=\/\.//g; s/\/gfwlist//g; /^server/d' > /tmp/temp_gfwlist2
 
-curl -sS https://raw.githubusercontents.com/Loyalsoldier/v2ray-rules-dat/release/gfw.txt > /tmp/temp_gfwlist3
+curl -sS https://ghproxy.com/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/gfw.txt > /tmp/temp_gfwlist3
 
 cat /tmp/temp_gfwlist1 /tmp/temp_gfwlist2 /tmp/temp_gfwlist3 | sort -u | sed 's/^\.*//g' > /tmp/temp_gfwlist
 cat /tmp/temp_gfwlist | sed 's/^/\./g' > /tmp/gfw.conf
